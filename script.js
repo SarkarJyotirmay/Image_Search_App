98306ce8e150db7054107ec5877639a3304046da
SarkarJyotirmay <jsarkar0201@gmail.com>
2025-03-11 11:06:54 +0530

    fixed bugs of images re-rendering and loader button

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
 let page = 1;
 
 let API = `https://api.unsplash.com/search/photos?page=${page}&query=`; //inout goes at the last
-let example_API = `https://api.unsplash.com/search/photos?page=${page}&query=avengers`
+let example_API = `https://api.unsplash.com/search/photos?page=${page}&query=avengers`;
 const ACESS_KEY = "pPf0b6TnHCt3ROnc3nzNo1RpP42QQl_Cqbt68X7Hq9c";
 const API_KEY = "rP1L4YhBiRq2peZCymz1NCi4CnZc7V-MkV-sXJ0z8KY";
 
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const resultsContainer = document.querySelector(".results-containainer");
-const loader = document.querySelector(".loader")
+const loader = document.querySelector(".loader");
 
 let images = []; // to store the fetched data
 
 showDefault(); // display dummy data fteched from api by default
-// showDefault Function show some dummy data here in this case avengers 
-// this function is an extension of the project read this function later if got confused
-async function showDefault(){
- let data =  await getData(example_API);
- data = data.results
-//  console.log(data);
- displayData(data);
- // loadmore increases the page number calls getData with updated API then calls displayData again with updated images array
- const loadMore = document.createElement("button");
- loadMore.innerText = "Load Mmore..."
- loadMore.classList.add("load-more");
-//  event on load more
- loadMore.addEventListener("click", async(e)=>{
-   page += 1;
-  example_API = `https://api.unsplash.com/search/photos?page=${page}&query=avengers`;
-  let data =  await getData(example_API);
- data = data.results
- console.log(data);
- displayData(data);
- })
 
- loader.append(loadMore) // append loadeMore to loader div 
+//! show deafult function
+// showDefault Function show some dummy data here in this case avengers
+// this function is an extension of the project read this function later if got confused
+async function showDefault() {
+  let data = await getData(example_API);
+  data = data.results;
+  images = data;
+  //  console.log(data);
+  displayData(images);
+  // loadmore increases the page number calls getData with updated API then calls displayData again with updated images array
+  const loadMore = document.createElement("button");
+  loadMore.innerText = "Load Mmore...";
+  loadMore.classList.add("load-more");
+  //  event on load more
+  loadMore.addEventListener("click", async (e) => {
+    page += 1;
+    example_API = `https://api.unsplash.com/search/photos?page=${page}&query=avengers`;
+    let data = await getData(example_API);
+    images = images.concat(data.results);
+    // images.splice(0,10); // not needed found on debugging
+    console.log(images);
+
+    displayData(images);
+  });
+  loader.innerHTML = "";
+  loader.append(loadMore); // append loadeMore to loader div
 }
 
-// on submit the form fetches data by using getData then calls display data function with fetched data 
-// > display searched images and add a loadmore bbutton 
+//! form submission
+// on submit the form fetches data by using getData then calls display data function with fetched data
+// > display searched images and add a loadmore bbutton
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const query = input.value;
@@ -47,25 +53,29 @@ form.addEventListener("submit", async (e) => {
   // console.log(images);
   displayData(images); //display images
 
-// on submit of form there will be a load more button arival
-// loadmore increases the page number calls getData with updated API then calls displayData again with updated images array
+  // on submit of form there will be a load more button arival
+  // loadmore increases the page number calls getData with updated API then calls displayData again with updated images array
   const loadMore = document.createElement("button");
-  loadMore.innerText = "Load Mmore..."
+  loadMore.innerText = "Load Mmore...";
   loadMore.classList.add("load-more");
-  loadMore.addEventListener("click", async(e)=>{
+  //! event on loadMore button
+  loadMore.addEventListener("click", async (e) => {
     page += 1;
     API = `https://api.unsplash.com/search/photos?page=${page}&query=`; //inout goes at the last
     const query = input.value;
     const data = await getData(API + query);
-    images = data.results;
-    // console.log(images);
+    // fixing bug of rerendering of images
+    images = images.concat(images, data.results);
+    images.splice(0, 10);
+    console.log(images);
     displayData(images);
-  })
+  });
 
-  loader.append(loadMore) // append loadeMore to loader div 
+  loader.innerHTML = ""; //to fix multiple occurance of load more button
+  loader.append(loadMore); // append loadeMore to loader div
 });
 
-//! get data function > fetches data from API 
+//! get data function > fetches data from API
 async function getData(url) {
   const response = await fetch(url, {
     headers: {
@@ -76,10 +86,10 @@ async function getData(url) {
   return result;
 }
 
-//!  display data function > creates a div with image and anchor and show them at the end 
+//!  display data function > creates a div with image and anchor and show them at the end
 function displayData(arr) {
-  console.log(arr);
-  // resultsContainer.innerHTML = "";
+  // console.log(arr);
+  resultsContainer.innerHTML = "";
   const fragment = document.createDocumentFragment();
   arr.forEach((obj) => {
     const parent = document.createElement("div");
@@ -96,10 +106,9 @@ function displayData(arr) {
     parent.append(image, description);
     fragment.append(parent);
   });
- 
+
   resultsContainer.append(fragment);
 }
-
 
 /* 
 Flow >>>
@@ -111,4 +120,3 @@ dcall isplayData(arr)
 
 Extended : showDefault(exampleURL) > to show dummy data
 */
- 
